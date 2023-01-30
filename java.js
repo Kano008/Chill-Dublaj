5e62544310a361f77294f80af59b19222b7bb8ff
Ali Eren <81834211+Kano008@users.noreply.github.com>
2023-01-30 14:38:15 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,23 @@
+const welcomeScreen = document.getElementById("welcome-screen");
 const aboutButton = document.getElementById("about-button");
+const contactButton = document.getElementById("contact-button");
+const addressButton = document.getElementById("address-button");
+
 const aboutContent = document.getElementById("about-content");
+const contactContent = document.getElementById("contact-content");
+const addressContent = document.getElementById("address-content");
 
 aboutButton.addEventListener("click", function() {
+  welcomeScreen.style.display = "none";
   aboutContent.style.display = "block";
+});
+
+contactButton.addEventListener("click", function() {
+  welcomeScreen.style.display = "none";
+  contactContent.style.display = "block";
+});
+
+addressButton.addEventListener("click", function() {
+  welcomeScreen.style.display = "none";
+  addressContent.style.display = "block";
 });
